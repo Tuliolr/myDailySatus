dc5fe4b362745beef7c33756d1e9dc604bfbfbdb
Tulio <tuliolopesr@gmail.com>
2020-04-30 10:20:58 -0300

    subindo projeto no git

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/styles.css'
+
+const App = ({ Component, pageProps }) => {
+    return (
+    <div>
+        <h1>APP</h1>
+        <Component {...pageProps} />
+        
+        
+    </div>
+    )
+} 
+
+export default App
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Index = () => {
+    return (
+    <div>
+        <div className='bg-gray-200'>
+        <h1>
+            <img src='/logo.png' alt='Olá FSL' /> 
+        </h1>
+        </div>
+    </div>
+    )
+}
+
+export default Index
